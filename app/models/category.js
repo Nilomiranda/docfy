f02414ae0b175c7da97b7526ccac251399f1e95d
Danilo Miranda <nilomiranda3@gmail.com>
2018-07-11 22:17:20 -0300

    Adiciona destaque de sintax com o highlight.js e ajusta a conversao do texto escrito em markdown

--- app/models/category.js
+++ app/models/category.js
@@ -1,7 +1,24 @@
+const hljs = require('highlight.js');
+const md = require('markdown-it')({
+  highlight: (str, lang) => {
+    if (lang && hljs.getLanguage(lang)) {
+      return `<pre class="hljs"><code>${hljs.highlight(lang, str.trim(), true).value}</code></pre>`;
+    }
+
+    return `<pre class="hljs"><code>${md.utils.escapeHtml(str.trim())}</code></pre>`;
+  },
+});
+
 module.exports = (sequelize, DataTypes) => {
   const Category = sequelize.define('Category', {
     name: DataTypes.STRING,
     content: DataTypes.TEXT,
+  }, {
+    getterMethods: {
+      formattedContent() {
+        return md.render(this.content);
+      },
+    },
   });
 
   // criando a relacao com a tabela de projetos
